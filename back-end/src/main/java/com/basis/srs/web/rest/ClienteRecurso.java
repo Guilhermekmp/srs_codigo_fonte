bbf54fb7589f2a00b4d3bc3f66a51fdf2e2983d3
= <=>
2020-10-01 17:14:44 -0300

    implementando recurso

--- back-end/src/main/java/com/basis/srs/web/rest/ClienteRecurso.java
+++ back-end/src/main/java/com/basis/srs/web/rest/ClienteRecurso.java
@@ -1,28 +1,52 @@
 package com.basis.srs.web.rest;
 
+import com.basis.srs.dominio.Cliente;
 import com.basis.srs.servico.ClienteServico;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @Getter
 @Setter
+@RequestMapping(path = "/api/clientes")
 public class ClienteRecurso {
 
     private ClienteServico servico;
 
-    public void inserirCliente(){
+    @PostMapping
+    public ResponseEntity<Integer> criar(@RequestBody Cliente cliente){
+        return null;
     }
-    public void listarClientes(){
+    @GetMapping
+    public ResponseEntity<List<Cliente>> listar(){
+        return null;
     }
-    public void listarClienteId(){
+    @GetMapping(path = "{id}")
+    public ResponseEntity<Cliente> procurar(@PathVariable Integer id){
+        return null;
     }
-    public void atualizar(){
+
+    @PutMapping
+    public ResponseEntity<Cliente> atualizar(@RequestBody Cliente cliente){
+        return null;
     }
-    public void deletar(){
+    @DeleteMapping(path = "{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Integer id){
+        return null;
     }
 
 
