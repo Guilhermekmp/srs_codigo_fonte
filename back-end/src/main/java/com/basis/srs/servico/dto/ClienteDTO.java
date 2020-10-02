307107e35184cc65df1772581f97ea539a53685b
Andriws Miranda <andriws2001@gmail.com>
2020-10-02 09:01:26 -0300

    Correções

--- back-end/src/main/java/com/basis/srs/servico/dto/ClienteDTO.java
+++ back-end/src/main/java/com/basis/srs/servico/dto/ClienteDTO.java
@@ -1,17 +1,13 @@
 package com.basis.srs.servico.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@Getter
+@Getter @AllArgsConstructor
 public class ClienteDTO {
 
     private String nome;
     private String endereco;
     private String email;
 
-    public ClienteDTO(String nome, String endereco, String email) {
-        this.nome = nome;
-        this.endereco = endereco;
-        this.email = email;
-    }
 }
