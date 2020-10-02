2ba707516912c210390d23fc2de7e146fc294d36
guilherme <guilherme.maciel.ti@gmail.com>
2020-10-02 14:49:53 -0300

    Padronização Cliente

--- back-end/src/main/java/com/basis/srs/dominio/Cliente.java
+++ back-end/src/main/java/com/basis/srs/dominio/Cliente.java
@@ -12,25 +12,27 @@ import java.util.Date;
 @Table(name = "cliente")
 public class Cliente implements Serializable {
 
-    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sq_cliente")
+    @SequenceGenerator(name="sq_cliente", allocationSize = 1, sequenceName = "sq_cliente")
+    @Column(name = "id")
     private int id;
 
-    @Column(length = 255, name = "nome")
+    @Column( name = "nome")
     private String nome;
 
-    @Column(length = 11)
+    @Column(name ="cpf")
     private String cpf;
 
     @Column(name = "data_nasc")
     private Date dataNasc;
 
-    @Column(length = 255)
+    @Column(name ="endereco")
     private String endereco;
 
-    @Column(length = 12)
-    private String telefone;
+    @Column(name ="rg")
+    private String rg;
 
-    @Column(length = 255)
+    @Column(name ="email")
     private String email;
 
 }
