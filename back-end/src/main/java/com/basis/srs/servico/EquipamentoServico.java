b88c722a93ee9eef967f0e5c4b8d063b42c2466e
Andriws Miranda <andriws2001@gmail.com>
2020-10-02 17:11:15 -0300

    Criação Crud Equipamentos

--- back-end/src/main/java/com/basis/srs/servico/EquipamentoServico.java
+++ back-end/src/main/java/com/basis/srs/servico/EquipamentoServico.java
@@ -2,6 +2,8 @@ package com.basis.srs.servico;
 
 import com.basis.srs.dominio.Equipamento;
 import com.basis.srs.repositorio.EquipamentoRepositorio;
+import com.basis.srs.servico.dto.EquipamentoDTO;
+import com.basis.srs.servico.mapper.EquipamentoMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,24 +15,27 @@ import java.util.List;
 @RequiredArgsConstructor
 public class EquipamentoServico {
 
-    private EquipamentoRepositorio equipamentoRepositorio;
+    private final EquipamentoRepositorio equipamentoRepositorio;
+    private EquipamentoMapper equipamentoMapper;
 
-    public Equipamento cadastrarEquipamento(Equipamento novoEquipamento){
-        return null;
+    public EquipamentoDTO criar(EquipamentoDTO novoEquipamento){
+
+        Equipamento equip = equipamentoRepositorio.save(equipamentoMapper.toEntity(novoEquipamento));
+        EquipamentoDTO equipDTO = equipamentoMapper.toDto(equip);
+        return equipDTO;
     }
 
-    public List<Equipamento> buscarTodos(){
-        return null;
+    public List<EquipamentoDTO> listar(){
+        List<EquipamentoDTO> dtoEquips = equipamentoMapper.toDto(equipamentoRepositorio.findAll());
+        return dtoEquips;
     }
 
-    public Equipamento buscarId(Integer id){
-        return null;
+    public EquipamentoDTO buscar(Integer id){
+        EquipamentoDTO dtoEquip = equipamentoMapper.toDto(equipamentoRepositorio.findById(id).orElse(null));
+        return dtoEquip;
     }
 
     public void deletar(Integer id){
-    }
-
-    public void atualizar(){
-
+         equipamentoRepositorio.deleteById(id);
     }
 }
