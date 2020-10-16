692609a1fc8d47ecf73ab5f4f78a7e61f8f5ebf3
Andriws Miranda <andriws2001@gmail.com>
2020-10-15 21:11:43 -0300

    Ajustes no FrontEnd de Equipamentos

--- frontend/src/app/equipamentos/equipamentos.component.ts
+++ frontend/src/app/equipamentos/equipamentos.component.ts
@@ -1,16 +1,43 @@
 import { EquipamentosService } from './equipamentos.service';
 import { Component, OnInit } from '@angular/core';
+import { style } from '@angular/animations';
+import { Equipamento } from './equipamento';
+import { ConfirmationService } from 'primeng';
 
 @Component({
   selector: 'app-equipamentos',
   templateUrl: './equipamentos.component.html',
-  styleUrls: ['./equipamentos.component.css']
+  styleUrls: ['./equipamentos.component.css'],
+  providers:[ConfirmationService]
 })
 export class EquipamentosComponent implements OnInit {
 
+  displayCreation:boolean = false;
   equipamentos = [];
+  equipamento = new Equipamento();
 
-  constructor(public equipamentosService: EquipamentosService) { 
+  constructor(private confirmationService: ConfirmationService, public equipamentosService: EquipamentosService) { 
+    
+  }
+
+  ngOnInit() {
+    this.listar();
+  }
+
+  getTipoEquipamento(equipamento: Equipamento): string{
+    switch(equipamento.idTipoEquipamento){
+      case 1:
+        return "Móvel";
+      case 2:
+        return "Eletrodoméstico";
+      case 3:
+        return "Informática";
+      default:
+        return null;
+    }
+  }
+
+  listar(){
     this.equipamentosService.listarEquipamentos().subscribe((data)=>{
       this.equipamentos = data;
       console.log(data);
@@ -20,12 +47,31 @@ export class EquipamentosComponent implements OnInit {
     })
   }
 
-  ngOnInit(): void {
+  deletar(id:any){
+    this.confirmationService.confirm({
+      message: 'Tem certeza que deseja excluir esse registro?',
+      accept: () => {
+        this.equipamentosService.deletar(id).subscribe(
+          r => {
+            this.listar()
+          }
+        )
+
+      }
+    })
   }
 
-  adicionarEquipamentos(){
-
+  salvar(){
+    console.log('equipamento', this.equipamento);
+    this.equipamentosService.salvar(this.equipamento).subscribe(response=>{
+      this.equipamento = response;
+    },
+    error=>
+    alert("ERROR!"))
   }
 
+  abrirPopUp(){
+    this.displayCreation = true;
+  }
 
 }
--- frontend/src/app/equipamentos/equipamentos.service.ts
+++ frontend/src/app/equipamentos/equipamentos.service.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs';
 import { Equipamento } from './equipamento';
 import { HttpClient } from '@angular/common/http';
 import { environment } from './../../environments/environment';
@@ -9,11 +10,27 @@ import { Injectable } from '@angular/core';
 export class EquipamentosService {
 
   private url = environment.apiUrl + '/equipamentos';
+  
+  equipamentos = [];
 
   constructor(private http:HttpClient) { }
+  
+  adicionarEquipamento(equipamento:Equipamento){
+    this.equipamentos.push(equipamento);
+    return this.equipamentos;
+  }
 
   listarEquipamentos(){
     return this.http.get<Equipamento[]>(this.url,{});
+  }
+
+  //Observable para transformar
+  deletar(id:any):Observable<any>{
+    return this.http.delete(this.url+"/"+id);
+  }
+
+  salvar(equipamento:Equipamento):Observable<Equipamento>{
+    return this.http.post<Equipamento>(this.url, equipamento);
   }
   
 }
