5be359b0023994fd35f4c6c68e8b7fe791e5f7db
guilherme <guilherme.maciel.ti@gmail.com>
2020-10-19 14:36:25 -0300

    Backend Reservas ManytoMany

--- back-end/src/main/java/com/basis/srs/servico/dto/ReservaEquipamentoDTO.java
+++ back-end/src/main/java/com/basis/srs/servico/dto/ReservaEquipamentoDTO.java
@@ -1,12 +1,13 @@
 package com.basis.srs.servico.dto;
 
+import java.io.Serializable;
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class ReservaEquipamentoDTO {
+public class ReservaEquipamentoDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Integer idReserva;
