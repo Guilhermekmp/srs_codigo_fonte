6751678f19b85bb381aa07aa161cbf47e644be0a
guilherme <guilherme.maciel.ti@gmail.com>
2020-10-27 08:52:10 -0300

    Reserva form post funcionando

--- frontend/src/app/reservas/reservas.module.ts
+++ frontend/src/app/reservas/reservas.module.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { SharedModule } from './../shared/shared.module';
 import { ReservasService } from './reservas.service';
 import { NgModule } from '@angular/core';
@@ -8,6 +9,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { ReservasFormComponent } from './reservas-form/reservas-form.component';
 import { CurrencyMaskModule } from 'ng2-currency-mask';
 import { ConfirmationService, InputNumberModule } from 'primeng';
+
 
 
 @NgModule({
